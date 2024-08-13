e62dcc08a9fa5bba497d5d702b83f065fef473e6
Muhammad Siyab <siyab.dev@gmail.com>
2024-08-13 07:32:15 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('test', function(){
+  return response()->json(['message' => 'Test route working!!!']);
+});
+
 require __DIR__.'/auth.php';
